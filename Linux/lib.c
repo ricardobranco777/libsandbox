3a75ecf997deba35b1cf0bb38e431d537ae163f3
Ricardo Branco <rbranco@suse.de>
2025-05-03 18:48:32 +0200

    Add landlock

--- Linux/lib.c
+++ Linux/lib.c
@@ -1,3 +1,4 @@
+#include <linux/landlock.h>
 #include <linux/seccomp.h>
 #include <linux/prctl.h>
 #include <sys/syscall.h>
@@ -6,12 +7,37 @@
 #include <errno.h>
 #include <err.h>
 
+static void
+landlock(void)
+{
+	struct landlock_ruleset_attr ruleset_attr = {
+		.handled_access_fs = LANDLOCK_ACCESS_FS_READ_FILE | LANDLOCK_ACCESS_FS_READ_DIR,
+	};
+	int fd;
+
+	fd = syscall(SYS_landlock_create_ruleset, &ruleset_attr, sizeof(ruleset_attr), 0);
+	if (fd == -1) {
+		if (errno != ENOSYS)
+			warn("%s", "landlock_create_ruleset");
+		return;
+	}
+
+	if (syscall(SYS_landlock_restrict_self, fd, 0) == -1 && errno != ENOSYS)
+		warn("%s", "landlock_restrict_self");
+
+	while (close(fd) == -1 && errno == EINTR)
+		;
+}
+
 void
 sandbox0(void)
 {
 	if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) == -1)
 		warn("%s", "prctl: PR_SET_NO_NEW_PRIVS");
 
+	landlock();
+
+	/* XXX After this seccomp, only read, write, _exit & sigreturn is allowed */
 #if 1
 	if (syscall(SYS_seccomp, SECCOMP_SET_MODE_STRICT, 0, NULL) == -1 && errno != ENOSYS)
 		warn("%s", "seccomp");
