f99ffd1180f8601f394523a5acb94e4492270ae2
Ricardo Branco <rbranco@suse.de>
2025-05-03 18:38:18 +0200

    Use syscall for seccomp

--- Linux/lib.c
+++ Linux/lib.c
@@ -1,9 +1,10 @@
 #include <linux/seccomp.h>
 #include <linux/prctl.h>
+#include <sys/syscall.h>
 #include <sys/prctl.h>
+#include <unistd.h>
 #include <errno.h>
 #include <err.h>
-#include <stdio.h>
 
 void
 sandbox0(void)
@@ -11,6 +12,11 @@ sandbox0(void)
 	if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) == -1)
 		warn("%s", "prctl: PR_SET_NO_NEW_PRIVS");
 
+#if 1
+	if (syscall(SYS_seccomp, SECCOMP_SET_MODE_STRICT, 0, NULL) == -1 && errno != ENOSYS)
+		warn("%s", "seccomp");
+#else
 	if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_STRICT) == -1 && errno != ENOSYS)
 		warn("%s", "prctl: SECCOMP_MODE_STRICT");
+#endif
 }
